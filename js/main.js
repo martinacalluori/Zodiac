afa0d9f63d3564fa71642f9074e419366fa53756
Martina Calluori <martinacalluori@n3-31-152.dhcp.drexel.edu>
2017-03-07 13:42:34 -0500

    Changed icon positions

--- js/main.js
+++ js/main.js
@@ -228,32 +228,30 @@ var imgArray2 = row2.children;
 	}
 
 	switch (whichOne){
-		case 'clear':
+		case 'hurricane':
 			imgArray[0].classList.add('active');
-			outputArea.innerHTML = '<h2>August 23 - Stepember 22</h2> <h3>Clear Skies</h3> This means you are very reliable and intelligent. You like to make sure others are happy, and sometimes this can lead you to be hypercritical of yourself. Keep sight of your goals and with hard work it will all pay off.';
-			 playAudio('birds');
-
+			outputArea.innerHTML = '<h2>January 20 - February 18</h2> <h3>Hurricane</h3> Your emotions can leave people spinning sometimes, but when you let your true energetic side shine through people are more likely to catch your good vibes. As long as you keep your mind occupied you can always trust you are on the path to success.';
+			playAudio('hurr');
 			break;
-		case 'calm':
-				imgArray[1].classList.add('active');
-				outputArea.innerHTML = '<h2>April 20 - May 20</h2> <h3>Calm</h3> You are strong and reliable, and people will often look to you for support. You find that sometimes it is hard to find someone for you to depend on, but look to those around you and you could be suprised at how supportive they are.';
-				playAudio('clm');
-			break;
-		case 'wild':
-			imgArray[2].classList.add('active');
-			outputArea.innerHTML = '<h2>July 23 - August 22</h2> <h3>Wildfire</h3> This means that you are warm and love to be loved. You love being the center of attention, and but your big heart will always shine through. When socializing, your infectious energy can quickly alter the mood of those around you. ';
-			playAudio('fire');
-			break;
-
 		case 'flurry':
-			imgArray[3].classList.add('active');
+			imgArray[1].classList.add('active');
 			outputArea.innerHTML = '<h2>February 19 - March 20</h2> <h3>Flurries</h3> You are very connected to the arts and music, and can use that to make connections with others. Your generosity and compassion for others also helps you maintain healthy relationships. Your intuition will lead you to the right path';
 			playAudio('flurries');
 			break;
 		case 'sand':
-				imgArray[4].classList.add('active');
-				outputArea.innerHTML = '<h2>March 21 - April 19</h2> <h3>Sand Storm</h3> You are daring, spontaneous and energetic. There is a constant flow of energy surrounding you, which makes it hard for others to stay upset around you. You will be suprised at how much you can learn from being a little more patient with people.';
-				playAudio('sands');
+			imgArray[2].classList.add('active');
+			outputArea.innerHTML = '<h2>March 21 - April 19</h2> <h3>Sand Storm</h3> You are daring, spontaneous and energetic. There is a constant flow of energy surrounding you, which makes it hard for others to stay upset around you. You will be suprised at how much you can learn from being a little more patient with people.';
+			playAudio('sands');
+			break;
+		case 'calm':
+			imgArray[3].classList.add('active');
+			outputArea.innerHTML = '<h2>April 20 - May 20</h2> <h3>Calm</h3> You are strong and reliable, and people will often look to you for support. You find that sometimes it is hard to find someone for you to depend on, but look to those around you and you could be suprised at how supportive they are.';
+			playAudio('clm');
+			break;
+		case 'tornado':
+			imgArray[4].classList.add('active');
+			outputArea.innerHTML = '<h2>May 21 - June 20</h2> <h3>Tornado</h3> Tornados are caused by hot and cold air clashing, and this is what your emotions often feel like. You can be restless and indecisive, but also be quick witted and fun to be around. People in your life sometimes wonder who they will be meeting that day. ';
+			playAudio('wind');
 			break;
 		case 'flood':
 			imgArray[5].classList.add('active');
@@ -261,37 +259,37 @@ var imgArray2 = row2.children;
 			playAudio('floods');
 			break;
 
-		case 'sunny':
-
+		case 'wild':
 			imgArray2[0].classList.add('active');
-			outputArea.innerHTML = '<h2>November 22 - December 21</h2> <h3>Sunny Day</h3> <p>This means that you are bright and can shine light on almost every situation. Your playfulness eases even the most tense situations. Watch out though, this may be mistaken for carelessness.</p>';
-			playAudio('chimes');
+			outputArea.innerHTML = '<h2>July 23 - August 22</h2> <h3>Wildfire</h3> This means that you are warm and love to be loved. You love being the center of attention, and but your big heart will always shine through. When socializing, your infectious energy can quickly alter the mood of those around you. ';
+			playAudio('fire');
 			break;
-		case 'tornado':
-				imgArray2[1].classList.add('active');
-				outputArea.innerHTML = '<h2>May 21 - June 20</h2> <h3>Tornado</h3> Tornados are caused by hot and cold air clashing, and this is what your emotions often feel like. You can be restless and indecisive, but also be quick witted and fun to be around. People in your life sometimes wonder who they will be meeting that day. ';
-				playAudio('wind');
+		case 'clear':
+			imgArray2[1].classList.add('active');
+			outputArea.innerHTML = '<h2>August 23 - Stepember 22</h2> <h3>Clear Skies</h3> This means you are very reliable and intelligent. You like to make sure others are happy, and sometimes this can lead you to be hypercritical of yourself. Keep sight of your goals and with hard work it will all pay off.';
+			playAudio('birds');
 			break;
 		case 'windy':
 			imgArray2[2].classList.add('active');
 			outputArea.innerHTML = '<h2>September 23 - October 22</h2> <h3>Windy</h3> This means you are kind and compassionate, but often you try to put others first and because of this, you get easily overwhelmed and stressed. Try to stay away from people that add more stress to your life, and embrace the ones who lift you up.';
 			playAudio('torn');
 			break;
-		case 'storm':
-			imgArray2[3].classList.add('active');
-			outputArea.innerHTML = '<h2>December 22 - January 20 </h2> <h3>Storm</h3> <p>This means that you are shy, stubborn, and sometimes even a little gloomy. You appear calm in most situations, but your inner fog can come out when pushed to the limit.</p>';
-			playAudio('thunder');
-			break;
 		case 'rainbow':
-				imgArray2[4].classList.add('active');
+			imgArray2[3].classList.add('active');
 			outputArea.innerHTML = '<h2>October 23 - November 21</h2> <h3>Rainbow</h3> This means that you know what you want and you will do anything to get it. You are fierce and brave, and live your life to experience things to the fullest and dive into every emotion you feel fully.';
 			playAudio('somewhere');
 			break;
-		case 'hurricane':
+		case 'sunny':
+			imgArray2[4].classList.add('active');
+			outputArea.innerHTML = '<h2>November 22 - December 21</h2> <h3>Sunny Day</h3> <p>This means that you are bright and can shine light on almost every situation. Your playfulness eases even the most tense situations. Watch out though, this may be mistaken for carelessness.</p>';
+			playAudio('chimes');
+			break;
+		case 'storm':
 			imgArray2[5].classList.add('active');
-			outputArea.innerHTML = '<h2>January 20 - February 18</h2> <h3>Hurricane</h3> Your emotions can leave people spinning sometimes, but when you let your true energetic side shine through people are more likely to catch your good vibes. As long as you keep your mind occupied you can always trust you are on the path to success.';
-			playAudio('hurr');
-			break;	
+			outputArea.innerHTML = '<h2>December 22 - January 20 </h2> <h3>Storm</h3> <p>This means that you are shy, stubborn, and sometimes even a little gloomy. You appear calm in most situations, but your inner fog can come out when pushed to the limit.</p>';
+			playAudio('thunder');
+			break;
+	
 	}
 }
 
